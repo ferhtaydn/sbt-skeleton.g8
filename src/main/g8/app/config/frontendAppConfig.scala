16a98425bdeb443bc7bdca4fdf932d13acf85911
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2017-01-03 15:54:34 +0000

    use hmrc libarary

--- src/main/g8/app/config/frontendAppConfig.scala
+++ src/main/g8/app/config/frontendAppConfig.scala
@@ -12,7 +12,7 @@ trait AppConfig {
 
 object FrontendAppConfig extends AppConfig with ServicesConfig {
 
-  private def loadConfig(key: String) = configuration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
+  //private def loadConfig(key: String) = configuration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
   private val contactHost = configuration.getString(s"contact-frontend.host").getOrElse("")
   private val contactFormServiceIdentifier = "MyService"
