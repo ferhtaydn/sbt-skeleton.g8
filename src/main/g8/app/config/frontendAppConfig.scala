f9225c357c470d112cbada3453ca2ad0f581f87b
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2017-01-03 15:58:57 +0000

    use hmrc libarary

--- src/main/g8/app/config/frontendAppConfig.scala
+++ src/main/g8/app/config/frontendAppConfig.scala
@@ -18,6 +18,5 @@ object FrontendAppConfig extends AppConfig with ServicesConfig {
 
   override lazy val analyticsToken = loadConfig(s"google-analytics.token")
   override lazy val analyticsHost = loadConfig(s"google-analytics.host")
-  override lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
-  override lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
+
 }
